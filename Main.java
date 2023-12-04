3996448f61f937cc39c04d056b4d9945e5ad0026
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-12-04 00:33:25 +0000

    practice

---
+++ Main.java
@@ -0,0 +1,25 @@
+// public class Main {
+//   public static void main(String[] args) {
+//     String word1 = "こんにちは。";
+//     String word2 = "私はjavaです。";
+//     String word3 = word1 + word2;
+//     System.out.println(word3);
+//   }
+// }
+
+// public class Main {
+//   public static void main(String[] args){
+//     String word = "こんにちは。私はjavaです。";
+//     word = word.replace("java", "いろは");
+//     System.out.println(word);
+//   }
+// }
+
+public class Main {
+  public static void main(String[] args){
+    int a = 10;
+    int b = 20;
+    String question = a + "x" + b + "=?";
+    System.out.println(question);
+  }
+}
