f6ad69c4ff7c3191c206cba3a5fce8ca5aa9baec
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-12-06 00:42:32 +0000

    java学習

--- Main.java
+++ Main.java
@@ -15,11 +15,62 @@
 //   }
 // }
 
-public class Main {
-  public static void main(String[] args){
-    int a = 10;
-    int b = 20;
-    String question = a + "x" + b + "=?";
-    System.out.println(question);
-  }
-}
+// public class Main {
+//   public static void main(String[] args){
+//     int a = 10;
+//     int b = 20;
+//     String question = a + "x" + b + "=?";
+//     System.out.println(question);
+//   }
+// }
+
+
+// import java.util.Random;
+// public class Main {
+//   public static void main(String[] args) {
+//     Random rnd = new Random(); //    ランダムな数を出す部品
+//     System.out.println(rnd.nextInt(10)); //    0~9のランダムな数を表示
+//   }
+// }
+
+
+
+// import java.util.Random;
+// public class Main {
+//   public static void main(String[] args) {
+//     Random rnd = new Random(); //    ランダムな数を出す部品
+//     int a = rnd.nextInt(100); //      0~99のランダムな数値を出す
+//     int b = rnd.nextInt(100); //      0~99のランダムな数値を出す
+//     String question = a + "x" + b "=?"; //    つなげて変数questionに入れる
+//     System.out.println(question); //    変数questionの文字列を表示
+//   }
+// }
+
+
+// public class Main {
+//   public static void main(String[] args) {
+//     double doubleA = 1234.5678; //   double型の変数doubleAに値を入れる
+//     int  intA = 12345; //    int型の変数intAに値を入れる
+//     doubleA = intA;
+//     System.out.println(doubleA);
+//   }
+// }
+
+
+
+// public class Main {
+//   public static void main(String[] args) {
+//     double doubleA = 1234.5678; //   double型の変数doubleAに値を入れる
+//     int  intA = 12345; //    int型の変数intAに値を入れる
+//     intA = (int)doubleA; //    doubleAの値をキャスト変換してintAに入れる
+//     System.out.println(intA);
+//   }
+// }
+
+
+// public class Main {
+//   public static void main(String[] args) {
+//     String [] lunch = {"スペシャルハンバーガー", "４種のチーズピッツァ", "ローストチキンサンド", "生ハムサンド"}; //    ４つの要素（文字列）を収めた配列
+//     System.out.println(lunch[2]); //    2番の要素を取り出して表示
+//   }
+// }
