8c35a08d21e8e5abaab7222b8a9b5ff992e472b7
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-12-12 23:46:31 +0000

    java学習

--- Main.java
+++ Main.java
@@ -98,15 +98,70 @@
 // }
 
 
-import java.util.Random;
+// import java.util.Random;
+// public class Main {
+//   public static void main(String[] args) {
+//     Random rnd = new Random();     //ランダムな数を出す部品
+//     int score = rnd.nextInt(100);   // 100までのランダムな数
+//     if (score >= 90) {    // 90以上なら当たり
+//         System.out.println("当たり！");
+//     } else {
+//         System.out.println("ハズレ");
+//     }
+//   }
+// }
+
+// public class Main {
+//   public static void main(String[] args) {
+//     for(int i = 0; i <= 9; i++) {   //変数iは０から９までを足していく
+//       System.out.println("5 X " + i + " = " + (5 * i));   //変数iを含めた掛け算の式。5X0〜5X9の１０個の掛け算をする
+//     }
+//   }
+// }
+
+// import java.util.Random;
+// public class Main {
+//   public static void main(String[] args) {
+//     Random rnd = new Random();     //ランダムな数を出す部品
+//     int answer = rnd.nextInt(10);   //かける数
+//     String question ="";
+//     for(int i = 0; i <= 9; i++) {
+//       //問題文変数に[answer*i]を足していきます
+//       question = question + "[" + (answer * i) + "]";
+//     }
+//     System.out.println("何の段の九九でしょう？");
+//     System.out.println(question);
+//     System.out.println("答え：" +answer);
+//   }
+// }
+
+
+// public class Main {
+//   public static void main(String[] args) {
+//     int [] scorelist= {64, 100, 78, 80, 72};  //配列
+//     for(int i:scorelist) {   //配列の全ての要素について表示することを繰り返す
+//       System.out.println(i);
+//     }
+//   }
+// }
+
+// public class Main {
+//   public static void main(String[] args) {
+//     int [] scorelist= {64, 100, 78, 80, 72};  //配列
+//     int total = 0;    //0にリセット
+//     for(int i:scorelist) {   //配列の全ての要素について表示することを繰り返す
+//       total += i;
+//     }
+//     System.out.println(total);    //変数totalの値を表示
+//   }
+// }
+
 public class Main {
   public static void main(String[] args) {
-    Random rnd = new Random();     //ランダムな数を出す部品
-    int score = rnd.nextInt(100);   // 100までのランダムな数
-    if (score >= 90) {    // 90以上なら当たり
-        System.out.println("当たり！");
-    } else {
-        System.out.println("ハズレ");
+    for(int i = 0; i <= 9; i++) {
+      for(int j = 0; j <= 9; j++) {
+        System.out.println(i+"x"+j+"="+(i*j));
+      }
     }
   }
 }
